25e42292c6c8de9d60eefa2a4fa7a72c1aba21d3
Afroz Alam <afrozalam@Afrozs-MacBook-Pro.local>
2018-11-25 15:10:15 +0530

    more flexibile approache in multipart upload

--- packages/@uppy/aws-s3-multipart/src/MultipartUploader.js
+++ packages/@uppy/aws-s3-multipart/src/MultipartUploader.js
@@ -1,290 +1,319 @@
-const MB = 1024 * 1024
+const MB = 1024 * 1024;
 
 const defaultOptions = {
   limit: 1,
-  onStart () {},
-  onProgress () {},
-  onPartComplete () {},
-  onSuccess () {},
-  onError (err) {
-    throw err
+  onStart() {},
+  onProgress() {},
+  onPartComplete() {},
+  onSuccess() {},
+  onError(err) {
+    throw err;
   }
-}
+};
 
-function remove (arr, el) {
-  const i = arr.indexOf(el)
-  if (i !== -1) arr.splice(i, 1)
+function remove(arr, el) {
+  const i = arr.indexOf(el);
+  if (i !== -1) arr.splice(i, 1);
 }
 
 class MultipartUploader {
-  constructor (file, options) {
-    this.options = Object.assign({}, defaultOptions, options)
-    this.file = file
+  constructor(file, options) {
+    this.options = Object.assign({}, defaultOptions, options);
+    this.file = file;
 
-    this.key = this.options.key || null
-    this.uploadId = this.options.uploadId || null
-    this.parts = this.options.parts || []
+    this.key = this.options.key || null;
+    this.uploadId = this.options.uploadId || null;
+    this.parts = this.options.parts || [];
 
-    this.isPaused = false
-    this.chunks = null
-    this.chunkState = null
-    this.uploading = []
-
-    this._initChunks()
+    this.isPaused = false;
+    this.chunks = null;
+    this.chunkState = null;
+    this.uploading = [];
+    this.chunkSize = this.options.chunkSize || 5 * MB;
+    this._initChunks();
   }
 
-  _initChunks () {
-    const chunks = []
-    const chunkSize = Math.max(Math.ceil(this.file.size / 10000), 5 * MB)
+  _initChunks() {
+    const chunks = [];
+    const chunkSize = Math.max(Math.ceil(this.file.size / 10000), this.chunkSize);
 
     for (let i = 0; i < this.file.size; i += chunkSize) {
-      const end = Math.min(this.file.size, i + chunkSize)
-      chunks.push(this.file.slice(i, end))
+      const end = Math.min(this.file.size, i + chunkSize);
+      chunks.push(this.file.slice(i, end));
     }
 
-    this.chunks = chunks
+    this.chunks = chunks;
     this.chunkState = chunks.map(() => ({
       uploaded: 0,
       busy: false,
       done: false
-    }))
+    }));
   }
 
-  _createUpload () {
-    return Promise.resolve().then(() =>
-      this.options.createMultipartUpload()
-    ).then((result) => {
-      const valid = typeof result === 'object' && result &&
-        typeof result.uploadId === 'string' &&
-        typeof result.key === 'string'
-      if (!valid) {
-        throw new TypeError(`AwsS3/Multipart: Got incorrect result from 'createMultipartUpload()', expected an object '{ uploadId, key }'.`)
-      }
-      return result
-    }).then((result) => {
-      this.key = result.key
-      this.uploadId = result.uploadId
-
-      this.options.onStart(result)
-    }).then(() => {
-      this._uploadParts()
-    }).catch((err) => {
-      this._onError(err)
-    })
-  }
-
-  _resumeUpload () {
-    return Promise.resolve().then(() =>
-      this.options.listParts({
-        uploadId: this.uploadId,
-        key: this.key
-      })
-    ).then((parts) => {
-      parts.forEach((part) => {
-        const i = part.PartNumber - 1
-        this.chunkState[i] = {
-          uploaded: part.Size,
-          etag: part.ETag,
-          done: true
+  _createUpload() {
+    return Promise.resolve()
+      .then(() => this.options.createMultipartUpload(this.chunks.length))
+      .then(result => {
+        const valid =
+          typeof result === "object" &&
+          result &&
+          typeof result.uploadId === "string" &&
+          typeof result.key === "string";
+        if (!valid) {
+          throw new TypeError(
+            `AwsS3/Multipart: Got incorrect result from 'createMultipartUpload()', expected an object '{ uploadId, key }'.`
+          );
         }
-
-        // Only add if we did not yet know about this part.
-        if (!this.parts.some((p) => p.PartNumber === part.PartNumber)) {
-          this.parts.push({
-            PartNumber: part.PartNumber,
-            ETag: part.ETag
-          })
-        }
+        return result;
       })
-      this._uploadParts()
-    }).catch((err) => {
-      this._onError(err)
-    })
+      .then(result => {
+        this.key = result.key;
+        this.uploadId = result.uploadId;
+        this.parts = result.parts;
+        this.completeURI = result.complete;
+        this.abortURI = result.abort;
+        this.options.onStart(result);
+      })
+      .then(() => {
+        this._uploadParts();
+      })
+      .catch(err => {
+        this._onError(err);
+      });
   }
 
-  _uploadParts () {
-    if (this.isPaused) return
+  _resumeUpload() {
+    this._uploadParts();
+    // return Promise.resolve().then(() =>
+    //   this.options.listParts({
+    //     uploadId: this.uploadId,
+    //     key: this.key
+    //   })
+    // ).then((parts) => {
+    //   parts.forEach((part) => {
+    //     const i = part.PartNumber - 1
+    //     this.chunkState[i] = {
+    //       uploaded: part.Size,
+    //       etag: part.ETag,
+    //       done: true
+    //     }
 
-    const need = this.options.limit - this.uploading.length
-    if (need === 0) return
+    //     // Only add if we did not yet know about this part.
+    //     if (!this.parts.some((p) => p.PartNumber === part.PartNumber)) {
+    //       this.parts.push({
+    //         PartNumber: part.PartNumber,
+    //         ETag: part.ETag
+    //       })
+    //     }
+    //   })
+    //   this._uploadParts()
+    // }).catch((err) => {
+    //   this._onError(err)
+    // })
+  }
+
+  _uploadParts() {
+    if (this.isPaused) return;
+
+    const need = this.options.limit - this.uploading.length;
+    if (need === 0) return;
 
     // All parts are uploaded.
-    if (this.chunkState.every((state) => state.done)) {
-      this._completeUpload()
-      return
+    if (this.chunkState.every(state => state.done)) {
+      this._completeUpload();
+      return;
     }
 
-    const candidates = []
+    const candidates = [];
     for (let i = 0; i < this.chunkState.length; i++) {
-      const state = this.chunkState[i]
-      if (state.done || state.busy) continue
+      const state = this.chunkState[i];
+      if (state.done || state.busy) continue;
 
-      candidates.push(i)
+      candidates.push(i);
       if (candidates.length >= need) {
-        break
+        break;
       }
     }
 
-    candidates.forEach((index) => {
-      this._uploadPart(index)
-    })
+    candidates.forEach(index => {
+      this._uploadPart(index);
+    });
   }
 
-  _uploadPart (index) {
-    const body = this.chunks[index]
-    this.chunkState[index].busy = true
-
-    return Promise.resolve().then(() =>
-      this.options.prepareUploadPart({
-        key: this.key,
-        uploadId: this.uploadId,
-        body,
-        number: index + 1
-      })
-    ).then((result) => {
-      const valid = typeof result === 'object' && result &&
-        typeof result.url === 'string'
-      if (!valid) {
-        throw new TypeError(`AwsS3/Multipart: Got incorrect result from 'prepareUploadPart()', expected an object '{ url }'.`)
+  _uploadPart(index) {
+    const body = this.chunks[index];
+    this.chunkState[index].busy = true;
+    return Promise.resolve().then(() => {
+      try {
+        const url = this.parts[index].url;
+        this._uploadPartBytes(index, url);
+      } catch (err) {
+        this._onError(err);
       }
-      return result
-    }).then(({ url }) => {
-      this._uploadPartBytes(index, url)
-    }, (err) => {
-      this._onError(err)
-    })
+    });
+
+    // return Promise.resolve().then(() =>
+    //   this.options.prepareUploadPart({
+    //     key: this.key,
+    //     uploadId: this.uploadId,
+    //     body,
+    //     number: index + 1
+    //   })
+    // ).then((result) => {
+    //   const valid = typeof result === 'object' && result &&
+    //     typeof result.url === 'string'
+    //   if (!valid) {
+    //     throw new TypeError(`AwsS3/Multipart: Got incorrect result from 'prepareUploadPart()', expected an object '{ url }'.`)
+    //   }
+    //   return result
+    // }).then(({ url }) => {
+    //   this._uploadPartBytes(index, url)
+    // }, (err) => {
+    //   this._onError(err)
+    // })
   }
 
-  _onPartProgress (index, sent, total) {
-    this.chunkState[index].uploaded = sent
+  _onPartProgress(index, sent, total) {
+    this.chunkState[index].uploaded = sent;
 
-    const totalUploaded = this.chunkState.reduce((n, c) => n + c.uploaded, 0)
-    this.options.onProgress(totalUploaded, this.file.size)
+    const totalUploaded = this.chunkState.reduce((n, c) => n + c.uploaded, 0);
+    this.options.onProgress(totalUploaded, this.file.size);
   }
 
-  _onPartComplete (index, etag) {
-    this.chunkState[index].etag = etag
-    this.chunkState[index].done = true
+  _onPartComplete(index, etag) {
+    this.chunkState[index].etag = etag;
+    this.chunkState[index].done = true;
 
     const part = {
       PartNumber: index + 1,
       ETag: etag
-    }
-    this.parts.push(part)
+    };
+    this.parts.push(part);
 
-    this.options.onPartComplete(part)
+    this.options.onPartComplete(part);
 
-    this._uploadParts()
+    this._uploadParts();
   }
 
-  _uploadPartBytes (index, url) {
-    const body = this.chunks[index]
-    const xhr = new XMLHttpRequest()
-    xhr.open('PUT', url, true)
-    xhr.responseType = 'text'
+  _uploadPartBytes(index, url) {
+    const body = this.chunks[index];
+    const xhr = new XMLHttpRequest();
+    xhr.open("PUT", url, true);
+    xhr.responseType = "text";
 
-    this.uploading.push(xhr)
+    this.uploading.push(xhr);
 
-    xhr.upload.addEventListener('progress', (ev) => {
-      if (!ev.lengthComputable) return
+    xhr.upload.addEventListener("progress", ev => {
+      if (!ev.lengthComputable) return;
 
-      this._onPartProgress(index, ev.loaded, ev.total)
-    })
+      this._onPartProgress(index, ev.loaded, ev.total);
+    });
 
-    xhr.addEventListener('abort', (ev) => {
-      remove(this.uploading, ev.target)
-      this.chunkState[index].busy = false
-    })
+    xhr.addEventListener("abort", ev => {
+      remove(this.uploading, ev.target);
+      this.chunkState[index].busy = false;
+    });
 
-    xhr.addEventListener('load', (ev) => {
-      remove(this.uploading, ev.target)
-      this.chunkState[index].busy = false
+    xhr.addEventListener("load", ev => {
+      remove(this.uploading, ev.target);
+      this.chunkState[index].busy = false;
 
       if (ev.target.status < 200 || ev.target.status >= 300) {
-        this._onError(new Error('Non 2xx'))
-        return
+        this._onError(new Error("Non 2xx"));
+        return;
       }
 
-      this._onPartProgress(index, body.size, body.size)
+      this._onPartProgress(index, body.size, body.size);
 
       // NOTE This must be allowed by CORS.
-      const etag = ev.target.getResponseHeader('ETag')
+      const etag = ev.target.getResponseHeader("ETag");
       if (etag === null) {
-        this._onError(new Error('AwsS3/Multipart: Could not read the ETag header. This likely means CORS is not configured correctly on the S3 Bucket. Seee https://uppy.io/docs/aws-s3-multipart#S3-Bucket-Configuration for instructions.'))
-        return
+        this._onError(
+          new Error(
+            "AwsS3/Multipart: Could not read the ETag header. This likely means CORS is not configured correctly on the S3 Bucket. Seee https://uppy.io/docs/aws-s3-multipart#S3-Bucket-Configuration for instructions."
+          )
+        );
+        return;
       }
 
-      this._onPartComplete(index, etag)
-    })
+      this._onPartComplete(index, etag);
+    });
 
-    xhr.addEventListener('error', (ev) => {
-      remove(this.uploading, ev.target)
-      this.chunkState[index].busy = false
+    xhr.addEventListener("error", ev => {
+      remove(this.uploading, ev.target);
+      this.chunkState[index].busy = false;
 
-      const error = new Error('Unknown error')
-      error.source = ev.target
-      this._onError(error)
-    })
+      const error = new Error("Unknown error");
+      error.source = ev.target;
+      this._onError(error);
+    });
 
-    xhr.send(body)
+    xhr.send(body);
   }
 
-  _completeUpload () {
+  _completeUpload() {
     // Parts may not have completed uploading in sorted order, if limit > 1.
-    this.parts.sort((a, b) => a.PartNumber - b.PartNumber)
+    this.parts.sort((a, b) => a.PartNumber - b.PartNumber);
 
-    return Promise.resolve().then(() =>
-      this.options.completeMultipartUpload({
-        key: this.key,
-        uploadId: this.uploadId,
-        parts: this.parts
-      })
-    ).then((result) => {
-      this.options.onSuccess(result)
-    }, (err) => {
-      this._onError(err)
-    })
+    return Promise.resolve()
+      .then(() =>
+        this.options.completeMultipartUpload({
+          url: this.completeURI,
+          key: this.key,
+          uploadId: this.uploadId,
+          parts: this.parts
+        })
+      )
+      .then(
+        result => {
+          this.options.onSuccess(result);
+        },
+        err => {
+          this._onError(err);
+        }
+      );
   }
 
-  _abortUpload () {
+  _abortUpload() {
     this.uploading.slice().forEach(xhr => {
-      xhr.abort()
-    })
+      xhr.abort();
+    });
     this.options.abortMultipartUpload({
+      url: this.abortURI,
       key: this.key,
       uploadId: this.uploadId
-    })
-    this.uploading = []
+    });
+    this.uploading = [];
   }
 
-  _onError (err) {
-    this.options.onError(err)
+  _onError(err) {
+    this.options.onError(err);
   }
 
-  start () {
-    this.isPaused = false
+  start() {
+    this.isPaused = false;
     if (this.uploadId) {
-      this._resumeUpload()
+      this._resumeUpload();
     } else {
-      this._createUpload()
+      this._createUpload();
     }
   }
 
-  pause () {
-    const inProgress = this.uploading.slice()
-    inProgress.forEach((xhr) => {
-      xhr.abort()
-    })
-    this.isPaused = true
+  pause() {
+    const inProgress = this.uploading.slice();
+    inProgress.forEach(xhr => {
+      xhr.abort();
+    });
+    this.isPaused = true;
   }
 
-  abort (opts = {}) {
-    const really = opts.really || false
+  abort(opts = {}) {
+    const really = opts.really || false;
 
-    if (!really) return this.pause()
+    if (!really) return this.pause();
 
-    this._abortUpload()
+    this._abortUpload();
   }
 }
 
-module.exports = MultipartUploader
+module.exports = MultipartUploader;
