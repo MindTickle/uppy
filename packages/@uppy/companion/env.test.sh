2ced2d415b48dba7d8f0a8b2d562088ad6160885
Amit Dugar <amit.dugar@mindtickle.com>
2018-11-30 00:55:32 +0530

    testing out something

--- packages/@uppy/companion/env.test.sh
+++ packages/@uppy/companion/env.test.sh
@@ -1,7 +1,7 @@
 export NODE_ENV="$TRACK"
 export COMPANION_PORT=80
 export COMPANION_DOMAIN="uppy-server.internal.macho.mindtickle.com:80"
-export COMPANION_SELF_ENDPOINT="uppy-server.internal.macho.mindtickle.com:80"
+export COMPANION_SELF_ENDPOINT="localhost:80"
 
 export COMPANION_PROTOCOL="http"
 export COMPANION_DATADIR="/tmp/uppy"
