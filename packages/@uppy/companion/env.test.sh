a9dee95bd69fbd35bb9c6042900e06d392152dd2
Amit Dugar <amit.dugar@mindtickle.com>
2018-11-29 18:20:39 +0530

    changing port to 80

--- packages/@uppy/companion/env.test.sh
+++ packages/@uppy/companion/env.test.sh
@@ -1,7 +1,7 @@
 export NODE_ENV="$TRACK"
-export COMPANION_PORT=3020
-export COMPANION_DOMAIN="localhost:3020"
-export COMPANION_SELF_ENDPOINT="localhost:3020"
+export COMPANION_PORT=80
+export COMPANION_DOMAIN="localhost:80"
+export COMPANION_SELF_ENDPOINT="localhost:80"
 
 export COMPANION_PROTOCOL="http"
 export COMPANION_DATADIR="/tmp/uppy"
