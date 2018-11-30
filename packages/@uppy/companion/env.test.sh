4d3755411ebdde8fb744134b6e893d1f2cc9bdee
Amit Dugar <amit.dugar@mindtickle.com>
2018-11-30 20:25:46 +0530

    change domain urls

--- packages/@uppy/companion/env.test.sh
+++ packages/@uppy/companion/env.test.sh
@@ -1,7 +1,7 @@
 export NODE_ENV="$TRACK"
 export COMPANION_PORT=80
-export COMPANION_DOMAIN="uppy-server.internal.macho.mindtickle.com:80"
-export COMPANION_SELF_ENDPOINT="uppy-server.internal.macho.mindtickle.com:80"
-export COMPANION_PROTOCOL="http"
+export COMPANION_DOMAIN="uploadermt.macho.mindtickle.com/api/v1/mtuploader"
+export COMPANION_SELF_ENDPOINT="uploadermt.macho.mindtickle.com/api/v1/mtuploader"
+export COMPANION_PROTOCOL="https"
 export COMPANION_DATADIR="/tmp/uppy"
 export COMPANION_SECRET="secret"
