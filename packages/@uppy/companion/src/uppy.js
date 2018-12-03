505040bd33daa9845870446bcf5c0d084ec138c5
afrozMT <afroz.alam@mindtickle.com>
2018-12-03 22:08:17 +0530

    session save

--- packages/@uppy/companion/src/uppy.js
+++ packages/@uppy/companion/src/uppy.js
@@ -83,16 +83,7 @@ module.exports.app = (options = {}) => {
   }
 
   // add uppy options to the request object so it can be accessed by subsequent handlers.
-  app.use("*", getOptionsMiddleware(options), req => {
-    req.session.grant = req.session.grant || {};
-    if (!req.session.grant.state && req.query.state) {
-      console.log("inside new intercept....");
-      req.session.grant.dynamic = req.session.grant.dynamic || {};
-      req.session.grant.dynamic.state = req.query.state;
-      req.session.grant.state = req.query.state;
-      req.session.grant.provider = "google";
-    }
-  });
+  app.use("*", getOptionsMiddleware(options));
   app.use("/s3", s3(options.providerOptions.s3));
   app.use("/url", url());
 
@@ -204,16 +195,6 @@ module.exports.socket = server => {
 const interceptGrantErrorResponse = interceptor((req, res) => {
   return {
     isInterceptable: () => {
-      req.session.grant = req.session.grant || {};
-      if (!req.session.grant.state && req.query.state) {
-        console.log("inside intercept....");
-        req.session.grant.dynamic = req.session.grant.dynamic || {};
-        req.session.grant.dynamic.state = req.query.state;
-        req.session.grant.state = req.query.state;
-        req.session.grant.provider = "google";
-      }
-
-      console.log("SESSION=====", req.session, "HOST: ", req.host);
       // match grant.js' callback url
       return /^\/connect\/\w+\/callback/.test(req.path);
     },
