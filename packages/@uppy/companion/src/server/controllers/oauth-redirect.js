ad697879ea07b2b9f880e64ec0240bf97485f7db
afrozMT <afroz.alam@mindtickle.com>
2018-12-03 17:21:33 +0530

    session save

--- packages/@uppy/companion/src/server/controllers/oauth-redirect.js
+++ packages/@uppy/companion/src/server/controllers/oauth-redirect.js
@@ -1,26 +1,38 @@
-const qs = require('querystring')
-const parseUrl = require('url').parse
-const { hasMatch } = require('../helpers/utils')
-const oAuthState = require('../helpers/oauth-state')
+const qs = require("querystring");
+const parseUrl = require("url").parse;
+const { hasMatch } = require("../helpers/utils");
+const oAuthState = require("../helpers/oauth-state");
 
 /**
  *
  * @param {object} req
  * @param {object} res
  */
-module.exports = function oauthRedirect (req, res) {
+module.exports = function oauthRedirect(req, res) {
   if (!req.query.state) {
-    return res.status(400).send('Cannot find state param in reques')
+    return res.status(400).send("Cannot find state param in reques");
   }
-  const handler = oAuthState.getFromState(req.query.state, 'uppyInstance', req.uppy.options.secret)
-  const handlerHostName = parseUrl(handler).host
+  const handler = oAuthState.getFromState(req.query.state, "uppyInstance", req.uppy.options.secret);
+  const handlerHostName = parseUrl(handler).host;
 
   if (hasMatch(handlerHostName, req.uppy.options.server.validHosts)) {
-    const providerName = req.uppy.provider.authProvider
-    const params = qs.stringify(req.query)
-    const url = `${handler}/connect/${providerName}/callback?${params}`
-    return res.redirect(url)
+    const providerName = req.uppy.provider.authProvider;
+    const params = qs.stringify(req.query);
+
+    const url = `${handler}/connect/${providerName}/callback?${params}`;
+    if (req.session) {
+      req.session.save(function(err) {
+        if (err) console.error(err);
+        _finish(url);
+      });
+    } else {
+      _finish(url);
+    }
+
+    function _finish(path) {
+      return res.redirect(path);
+    }
   }
 
-  res.status(400).send('Invalid Host in state')
-}
+  res.status(400).send("Invalid Host in state");
+};
