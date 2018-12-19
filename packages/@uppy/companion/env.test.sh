02950ba50136f639dd42f6baa1e9fdf284d4f559
Amit Dugar <amit.dugar@mindtickle.com>
2018-12-19 13:18:40 +0530

    testing out something

--- packages/@uppy/companion/env.test.sh
+++ packages/@uppy/companion/env.test.sh
@@ -1,7 +1,7 @@
 export NODE_ENV="$TRACK"
 export COMPANION_PORT=80
-export COMPANION_DOMAIN="uploadermt.macho.mindtickle.com/api/v1/mtuploader"
-export COMPANION_SELF_ENDPOINT="uploadermt.macho.mindtickle.com/api/v1/mtuploader"
+export COMPANION_DOMAIN="uploadermt.macho.mindtickle.com"
+export COMPANION_SELF_ENDPOINT="uploadermt.macho.mindtickle.com"
 export COMPANION_PROTOCOL="https"
 export COMPANION_DATADIR="/tmp/uppy"
 export COMPANION_SECRET="secret"
