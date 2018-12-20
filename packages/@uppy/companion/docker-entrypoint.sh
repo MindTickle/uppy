84027dc3bef054aee526fc439812b7e122cb0837
Amit Dugar <amit.dugar@mindtickle.com>
2018-12-20 12:38:31 +0530

    testing out something

--- packages/@uppy/companion/docker-entrypoint.sh
+++ packages/@uppy/companion/docker-entrypoint.sh
@@ -4,8 +4,8 @@ set -x
 env \
 NODE_ENV="$TRACK" \
 COMPANION_PORT=80 \
-COMPANION_DOMAIN="uploadermt.macho.mindtickle.com" \
-COMPANION_SELF_ENDPOINT="uploadermt.macho.mindtickle.com" \
+COMPANION_DOMAIN="uploadermt.macho.mindtickle.com/api/v1/mtuploader" \
+COMPANION_SELF_ENDPOINT="uploadermt.macho.mindtickle.com/api/v1/mtuploader" \
 COMPANION_PROTOCOL="https" \
 COMPANION_DATADIR="/tmp/uppy" \
 COMPANION_SECRET="secret" \
